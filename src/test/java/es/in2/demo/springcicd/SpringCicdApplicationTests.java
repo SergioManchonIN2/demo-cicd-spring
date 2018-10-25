7b63e14001c8ece60a1e05c6c3f96cedecba2a08
Pau Gallardo <pau.gallardo@in2.es>
2018-10-25 21:40:54 +0200

    Init project

---
+++ src/test/java/es/in2/demo/springcicd/SpringCicdApplicationTests.java
@@ -0,0 +1,22 @@
+package es.in2.demo.springcicd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCicdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		// TODO
+	}
+
+	@Test
+	public void contextLoads2Test() {
+		// TODO
+	}
+
+}
