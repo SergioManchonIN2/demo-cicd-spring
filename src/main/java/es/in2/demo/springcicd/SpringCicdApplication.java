93fd9a39b6212bbf442cfd3ea27e42104b6bc09c
Pau Gallardo <pau.gallardo@in2.es>
2018-10-25 22:44:21 +0200

    ADD swagger support

--- src/main/java/es/in2/demo/springcicd/SpringCicdApplication.java
+++ src/main/java/es/in2/demo/springcicd/SpringCicdApplication.java
@@ -2,12 +2,44 @@ package es.in2.demo.springcicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringCicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCicdApplication.class, args);
 	}
+
+    /**
+     * Publish a bean to generate swagger2 endpoints
+     * @return a swagger configuration bean
+     */
+    @Bean
+    public Docket usersApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(usersApiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo usersApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Service User")
+                .version("1.0")
+                .license("Apache License Version 2.0")
+                .build();
+    }
 
 }
